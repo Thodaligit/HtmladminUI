2ea53191762d5893584c86452dafc407b6c6f3ee
Thodaligit <166012656+Thodaligit@users.noreply.github.com>
2024-07-29 19:37:36 +0530

    Add files via upload

---
+++ Models/DayBookBL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AdminUI.Models
+{
+    public class DayBookBL
+    {
+        [Key]
+        public long AccountNo { get; set; }
+        public string Date { get; set; }
+        public string AccountName { get; set; }
+        public string AccountDescription { get; set; }
+        public string AccountAmount { get; set; }
+        public bool IsNeedsToDelete { get; set; }
+    }
+}
---
+++ Models/DayBookBLVM.cs
@@ -0,0 +1,17 @@
+﻿using AdminUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminUI.Model
+{
+    public class DayBookBLVM
+    {
+        public DayBookBLVM()
+        {
+            ObjDayBookBLList = new List<DayBookBL>();
+        }
+        public List<DayBookBL> ObjDayBookBLList { get; set; }
+    }
+}
